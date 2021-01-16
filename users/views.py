fe4377c8930f5edec639797d60c97ef50ff311a9
kamula <kamulaisaac@gmail.com>
2021-01-16 21:18:12 +0300

    login view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import AccountAuthentication, userregistration
+# registration view
+# def registration_view(request):
+#     return
 
-# Create your views here.
+
+# login view
+def login_view(request):
+    context = {}
+    user = request.user
+    if user.is_authenticated:
+        # redirect to dashboard if user is valid
+        return redirect('dashboard')
+    if request.method == 'POST':
+        form = AccountAuthentication(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                # redirect to dashboard if user is 
+                return redirect('dashboard')
+    else:
+        form = AccountAuthentication
+    context['login_form'] = form
+    return render(request, 'accounts/login.html')
+
+# logout view
+
+
+
+# Dashboard
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
