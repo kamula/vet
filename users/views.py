78a230cf43a2018254b816bbef3c1ce6d456b5ee
kamula <kamulaisaac@gmail.com>
2021-01-17 11:30:59 +0300

    list all users except superuser

--- users/views.py
+++ users/views.py
@@ -3,12 +3,13 @@ from django.contrib.auth import login, authenticate, logout
 from .forms import AccountAuthentication, userregistration
 from django.contrib.auth.decorators import login_required
 from .counties import counties
+from .models import Users
 # registration view
 
 
 def registration_view(request):
     context = {
-        "counties":counties
+        "counties": counties
     }
     if request.POST:
         form = userregistration(request.POST)
@@ -26,10 +27,12 @@ def registration_view(request):
     return render(request, 'accounts/register.html', context)
 
 # Add user view
+
+
 @login_required(login_url='login')
 def add_user(request):
     context = {
-        "counties":counties
+        "counties": counties
     }
     if request.POST:
         form = userregistration(request.POST)
@@ -38,7 +41,7 @@ def add_user(request):
             email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password')
             account = authenticate(email=email, password=raw_password)
-            return redirect('dashboard')
+            return redirect('users')
         else:
             context['registrationform'] = form
     else:
@@ -63,7 +66,7 @@ def login_view(request):
             if user:
                 login(request, user)
                 # redirect to dashboard if user is
-                return redirect('dashboard')
+                return redirect('users')
     else:
         form = AccountAuthentication
     context['login_form'] = form
@@ -76,8 +79,19 @@ def logout_view(request):
     logout(request)
     return redirect('login')
 
+# users
+# redirect to login page if user is not logged in
+@login_required(login_url='login')
+def users_view(request):
+    users = Users.objects.order_by("-date_joined").exclude(is_superuser=True)
+    context = {
+        "users": users
+    }
+    return render(request, "accounts/users.html", context)
 
 # Dashboard
+
+
 @login_required(login_url='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
