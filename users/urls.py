6e653fda5b17266d98b3bd82ed1e989a2a21de1f
kamula <kamulaisaac@gmail.com>
2021-01-17 07:38:58 +0300

    register app in admin

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import dashboard,login_view,logout_view
+from .views import dashboard,login_view,registration_view,logout_view
 
 urlpatterns = [
     path('',login_view,name='login'),
+    path('register/',registration_view,name='register'),
     path('logout/',logout_view,name='logout'),
     path('dashboard/', dashboard, name = "dashboard")    
 ]
