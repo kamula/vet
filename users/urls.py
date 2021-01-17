9eab521cd5603ae5e7e490e010b7e3e8fe44124c
kamula <kamulaisaac@gmail.com>
2021-01-17 07:19:59 +0300

    users routes & login css

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
-from .views import dashboard
+from .views import dashboard,login_view,logout_view
 
 urlpatterns = [
+    path('',login_view,name='login'),
+    path('logout/',logout_view,name='logout'),
     path('dashboard/', dashboard, name = "dashboard")    
 ]
