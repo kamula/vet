4e1d9dba5b9f35c4bde3c1caebacf1bdc56c4ac2
kamula <kamulaisaac@gmail.com>
2021-01-18 08:24:44 +0300

    user serializer

---
+++ users/api/serializers.py
@@ -0,0 +1,34 @@
+from django.db.models.base import Model
+from rest_framework import serializers
+from users.models import Users
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(
+        style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        Model = Users
+        fields = ('name', 'email', 'county', 'id_no',
+                  'phone_number', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        user = Users(
+            name=self.validated_data['name'],
+            email=self.validated_data['email'],
+            county=self.validated_data['county'],
+            id_no=self.validated_data['id_no'],
+            phone_number=self.validated_data['phone_number']
+
+        )
+        password = self.validated_data['password1']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError(
+                {'password': 'passwords must match'})
+        user.set_password(password)
+        user.save()
+        return user
