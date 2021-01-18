ca052da8db3f40ab1445a0dae3aaab2d534a5a6f
kamula <kamulaisaac@gmail.com>
2021-01-18 10:40:29 +0300

    login url

--- users/api/urls.py
+++ users/api/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from django.urls.resolvers import URLPattern
 from .views import (registration_view)
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
-    path('register',registration_view,name='api_register')
+    path('register',registration_view,name='api_register'),
+    path('login',obtain_auth_token,name='api_login')
 ]
 
